f1d24f8f0bebcd83c3807d05317a78d3a4ae3a66
Glenn <glennpws@hotmail.co.uk>
2025-10-11 01:41:34 +0100

    Hazards can now do persistent damage over time.

--- PlatformerGame/Assets/Scripts/EnemiesAndHazards/Hazard.cs
+++ PlatformerGame/Assets/Scripts/EnemiesAndHazards/Hazard.cs
@@ -1,25 +1,46 @@
+using System.Collections;
 using UnityEngine;
 
 public class Hazard : MonoBehaviour
 {
     [Header("Damage")]
     [SerializeField] private float damageAmount = 25f;
+    [SerializeField] private float damageInterval;
+    private bool isPlayerInContact = false;
 
     [Header("Collision Settings")]
     [SerializeField] private bool destroyOnHit = false;
 
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        IDamageable damageable = other.GetComponent<IDamageable>();
-
-        if (damageable != null)
+        if (other.CompareTag("Player") && !isPlayerInContact)
         {
-            damageable.TakeDamage(damageAmount);
+            isPlayerInContact = true;
+            IDamageable damageableTarget = other.GetComponent<IDamageable>();
 
-            if (destroyOnHit)
+            if (damageableTarget != null)
             {
-                Destroy(gameObject);
+                StartCoroutine(RepeatingDamage(damageableTarget));
             }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && isPlayerInContact)
+        {
+            StopAllCoroutines();
+            isPlayerInContact = false;
+        }
+    }
+
+    private IEnumerator RepeatingDamage(IDamageable target)
+    {
+        while (isPlayerInContact)
+        {
+            target.TakeDamage(damageAmount);
+            yield return new WaitForSeconds(damageInterval);
         }
     }
 }
